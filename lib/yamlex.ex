4cccc1edf0b1f52625c87d7e9931c58ed81234cc
BennyHallett <benny.hallett@gmail.com>
2014-07-10 08:49:48 +1000

    Added module docs

--- lib/yamlex.ex
+++ lib/yamlex.ex
@@ -1,13 +1,50 @@
 defmodule Yamlex do
 
+  @moduledoc """
+  Yamlex is a YAML parser.
+
+  It is currently implemented as a wrapper around [yamerl](https://github.com/yakaz/yamerl),
+  but will be moved to a pure Elixir implementation in the future.
+
+  ## Usage
+
+  To parse a string containing YAML, use:
+  
+      Yamlex.parse_string "---\\nkey: value"
+
+  To parse a file containing YAML, use:
+  
+      Yamlex.parse_file "/path/to/file"
+
+  ## Note
+
+  `Yamlex.parse_file/1` will hang if the file contains just the YAML header, `---`, and no
+  other characters (i.e no trailing newline). This issue is handled in `Yamlex.parse_string/1`
+  but not when parsing files.
+  """
+
   def parse_string(text) when text == "---" do
     { :error, :parse }
   end
+  
+  @doc """
+  Parse a YAML string and convert it into a list of tuples
 
+  ## Usage
+
+      Yamlex.parse_string "---\\ntest: data\\none: 1"
+  """
   def parse_string(text) do
     { :ok, :yamerl_constr.string(text) }
   end
 
+  @doc """
+  Parse a file containing a YAML document and convert it into a list of tuples
+
+  ## Usage
+
+      Yamlex.parse_string "/path/to/file"
+  """
   def parse_file(path) do
     { :ok, :yamerl_constr.file(path) }
   end
